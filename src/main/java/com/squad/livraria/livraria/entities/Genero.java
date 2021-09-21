8f08e5c2c5c444d8182f5417221167afbfb7adeb
emersonnobre <egrnobre@gmail.com>
2021-09-21 09:58:21 -0400

    Criação do GeneroRepository, GeneroBiz, GeneroController

--- src/main/java/com/squad/livraria/livraria/entities/Genero.java
+++ src/main/java/com/squad/livraria/livraria/entities/Genero.java
@@ -1,4 +1,40 @@
 package com.squad.livraria.livraria.entities;
 
+import javax.persistence.*;
+
+@Entity
+@Table(name = "genero")
 public class Genero {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name="id", nullable = false)
+    private Integer id;
+
+    @Column(name="nome", nullable = false, length = 50)
+    private String nome;
+
+    public Genero() {}
+
+    public Genero(Integer id, String nome) {
+        this.id = id;
+        this.nome = nome;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
 }
