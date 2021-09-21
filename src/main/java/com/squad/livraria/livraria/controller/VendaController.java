90f1ee35bd94dc85a0333a80212534646325323f
Thiago M. Peres <tperes@itelinfo.com>
2021-09-21 10:15:49 -0400

    Criação de classes venda

--- src/main/java/com/squad/livraria/livraria/controller/VendaController.java
+++ src/main/java/com/squad/livraria/livraria/controller/VendaController.java
@@ -13,26 +13,7 @@ import com.squad.livraria.livraria.entities.Venda;
 public class VendaController {
 
 	
-	@GetMapping("listar")
-	public List<Venda> listar(){
-		return vendaRepositorio.findAll();
-	}
 	
-	@PostMapping("add")
-	public ArrayList<String> adicionar(@RequestBody Venda novoVenda) {
-		ArrayList<String> mensagens = new ArrayList<>();
-		VendaBiz validador = new VendaBiz(boiRepository,vendedorRepository,compradorRepository,leilaoRepository);
-		try {
-			validador.validarVenda(novoVenda);
-			vendaRepositorio.save(novoVenda);
-			vendaRepositorio.flush();
-			mensagens.add("Sucesso ao adicionar venda");
-		}catch(Exception error) {
-			mensagens.add("Erro ao adicionar venda");
-			mensagens.addAll(validador.getMsg());
-			mensagens.add(error.getMessage());
-		}
-		return mensagens;
 	}
 
-}
+
