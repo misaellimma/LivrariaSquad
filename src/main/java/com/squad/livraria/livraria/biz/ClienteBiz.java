0987c0db8290b11686d34ea66bf97a079cc67575
Marcus Vinicius R. Oliveira <moliveira@itelinfo.com>
2021-09-21 11:09:41 -0400

    Adicionado ClienteBiz

---
+++ src/main/java/com/squad/livraria/livraria/biz/ClienteBiz.java
@@ -0,0 +1,76 @@
+package com.squad.livraria.livraria.biz;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import com.squad.livraria.livraria.Mensagem;
+import com.squad.livraria.livraria.entities.Cliente;
+import com.squad.livraria.livraria.repositories.ClienteRepository;
+
+public class ClienteBiz {
+	
+	private Mensagem mensagens;
+	private ClienteRepository clienteRepository;
+	
+	public ClienteBiz() {}
+	
+	public ClienteBiz(ClienteRepository clienteRepository) {
+		super();
+		this.mensagens = new Mensagem();
+		this.clienteRepository = clienteRepository;
+	}
+		
+	public Mensagem getMensagens() {
+		return mensagens;
+	}
+
+	public void setMensagens(Mensagem mensagens) {
+		this.mensagens = mensagens;
+	}
+
+	public ClienteRepository getClienteRepository() {
+		return clienteRepository;
+	}
+
+	public void setClienteRepository(ClienteRepository clienteRepository) {
+		this.clienteRepository = clienteRepository;
+	}
+
+	public Boolean validar(Cliente cliente) {
+		
+		Boolean valido = true;
+		
+		if(cliente.getNome().isEmpty()) {
+			mensagens.mensagem.add("O nome do cliente não pode estar vazio");
+			valido = false;
+		}else if(cliente.getNome().length() > 50) {
+			mensagens.mensagem.add("O nome é muito grande, ele deve ter até 50 caracteres");
+			valido = false;
+		}
+		
+		if (cliente.getEmail() != null && cliente.getEmail().length() > 0) {
+	        String validacao = "^[\\w!#$%&’*+/=?`{|}~^-]+(?:\\.[\\w!#$%&’*+/=?`{|}~^-]+)*@(?:[a-zA-Z0-9-]+\\.)+[a-zA-Z]{2,6}$";
+	        Pattern pattern = Pattern.compile(validacao, Pattern.CASE_INSENSITIVE);
+	        Matcher matcher = pattern.matcher(cliente.getEmail());
+	        if (matcher.matches()) {
+	            valido = true;
+	        }else if(cliente.getEmail().isEmpty()) {
+	        	mensagens.mensagem.add("O nome do cliente não pode estar vazio");
+	        	valido = false;
+	        }else {
+	        	valido = false;
+	        	mensagens.mensagem.add("O formato de email informado não é válido");
+	        }
+	    }
+		
+		if(cliente.getNome().isEmpty()) {
+			mensagens.mensagem.add("O número de celular não pode estar vazio");
+			valido = false;
+		}else if(cliente.getNome().length() > 20) {
+			mensagens.mensagem.add("O nome é muito grande, ele deve ter até 20 caracteres");
+			valido = false;
+		}
+
+		return valido;
+	}
+}
--- src/main/java/com/squad/livraria/livraria/controller/ClienteController.java
+++ src/main/java/com/squad/livraria/livraria/controller/ClienteController.java
@@ -4,11 +4,15 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.squad.livraria.livraria.Mensagem;
+import com.squad.livraria.livraria.biz.ClienteBiz;
 import com.squad.livraria.livraria.entities.Cliente;
 import com.squad.livraria.livraria.repositories.ClienteRepository;
 
@@ -25,14 +29,32 @@ public class ClienteController {
 	}
 	
 	@PostMapping("incluir")
-	public String incluirCliente(@RequestBody Cliente cliente) {
+	public Mensagem incluirCliente(@RequestBody Cliente cliente) {
+		
+		ClienteBiz clienteBiz = new ClienteBiz(clienteRepository);
 		
 		try{
-			clienteRepository.save(cliente);
-			clienteRepository.flush();
-			return cliente.toString();
+			if(clienteBiz.validar(cliente)) {
+				clienteRepository.save(cliente);
+				clienteRepository.flush();
+				clienteBiz.getMensagens().mensagem.add("Cliente criado com sucesso");
+			}
+			
 		}catch(Exception e) {
-			return e.getMessage();
+			clienteBiz.getMensagens().mensagem.add("Erro ao Incluir: " + e.getMessage());
 		}
+		return clienteBiz.getMensagens();
 	}
+	
+    @PutMapping("alterar/{id}")
+    public String alterarCliente(@PathVariable Integer id, @RequestBody Cliente cliente) {
+        
+        try{
+            clienteRepository.save(cliente);
+            clienteRepository.flush();
+            return cliente.toString();
+        }catch(Exception e) {
+            return e.getMessage();
+        }
+    }
 }
