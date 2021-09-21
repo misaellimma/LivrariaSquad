4ff2c4af895f0c2e6c29585dbc6a8a83894876b2
emersonnobre <egrnobre@gmail.com>
2021-09-21 09:29:45 -0400

    commit do projeto configurado

---
+++ src/main/java/com/squad/livraria/livraria/controller/TestController.java
@@ -0,0 +1,16 @@
+package com.squad.livraria.livraria.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+
+	@GetMapping("oi")
+	public String get()
+	{
+		return "oi";
+	}
+	
+}
---
+++ src/test/java/com/squad/livraria/livraria/LivrariaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.squad.livraria.livraria;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LivrariaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
